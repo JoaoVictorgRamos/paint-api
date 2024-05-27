9abda92347bd1ed810c0a8ef997e87dae2cb4ac7
João Victor Ramos <joao.ramos@greenn.com.br>
2024-05-27 13:53:25 -0300

    fix-cors

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,17 @@ import express, { Application } from "express";
 // env
 import dotenv from "dotenv";
 dotenv.config();
+// cors
+const cors = require('cors');
 // routers
 import router from "./routes/index";
 // app
 const app: Application = express();
 app.use(express.json());
 app.use(router);
+app.use(cors({
+    origin: '*'
+  }));
 
 const PORT = process.env.PORT || 3000;
 
